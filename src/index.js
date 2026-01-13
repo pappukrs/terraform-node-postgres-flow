05f9d9858822e8f3d0d6d2290da71e4f17cff946
pappukrs <digitalpappu101@gmail.com>
2026-01-13 10:28:40 +0530

    issue fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
